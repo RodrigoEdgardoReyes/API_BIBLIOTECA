b32eeda7b3908dc8cebf2c8fc37a189b3a0112b5
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-20 20:52:46 -0600

    entidad de prestamos :tada::sparkles:

--- src/prestamos/entities/prestamo.entity.ts
+++ src/prestamos/entities/prestamo.entity.ts
@@ -1,6 +1,7 @@
 import { Libro } from 'src/libros/entities/libro.entity';
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
+// Entidad prestamo
 @Entity()
 export class Prestamo {
   @PrimaryGeneratedColumn()
