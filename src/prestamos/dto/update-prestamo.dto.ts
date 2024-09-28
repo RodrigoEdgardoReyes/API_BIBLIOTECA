e40aac9011819b65c67ae662f6a59a9ac370957b
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-27 20:53:00 -0600

    update prestamo

--- src/prestamos/dto/update-prestamo.dto.ts
+++ src/prestamos/dto/update-prestamo.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePrestamoDto } from './create-prestamo.dto';
 
+// Falta
 export class UpdatePrestamoDto extends PartialType(CreatePrestamoDto) {}
