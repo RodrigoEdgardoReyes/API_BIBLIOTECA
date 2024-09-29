298655939b94ce1303736a2984727b4c49428f87
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-28 23:16:11 -0600

    update :tada::sparkles:

--- src/prestamos/dto/update-prestamo.dto.ts
+++ src/prestamos/dto/update-prestamo.dto.ts
@@ -2,4 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreatePrestamoDto } from './create-prestamo.dto';
 
 // Falta
-export class UpdatePrestamoDto extends PartialType(CreatePrestamoDto) {}
+export class UpdatePrestamoDto{}
