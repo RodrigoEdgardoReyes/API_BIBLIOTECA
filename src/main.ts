7a6a10550223c0e6c24b1369c4814511659da7a0
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-28 18:53:59 -0600

    main :tada::sparkles:

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Permitir cors desde el frontend
+  app.enableCors({
+    origin: 'http://localhost:5173'
+  });
+
   // Puerto corriendo 3000
   await app.listen(3000);
 }
