ec6f66bcc11b61e18316a04b1116d3fcef8290ec
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-28 18:56:46 -0600

    modulo prestamo :tada::sparkles:

--- src/prestamos/prestamos.module.ts
+++ src/prestamos/prestamos.module.ts
@@ -3,9 +3,10 @@ import { PrestamosService } from './prestamos.service';
 import { PrestamosController } from './prestamos.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Prestamo } from './entities/prestamo.entity';
+import { Libro } from 'src/libros/entities/libro.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Prestamo])],
+  imports: [TypeOrmModule.forFeature([Prestamo, Libro])],
   controllers: [PrestamosController],
   providers: [PrestamosService],
 })
